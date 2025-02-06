b910140aaee48d4467b04825277bed6f4294e30a
wr1159 <chuweiron18@gmail.com>
2025-02-06 18:41:40 +0000

    SAMServiceManager: Scaffold functions

---
+++ contracts/src/SAMServiceManager.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {ISignatureUtils} from "eigenlayer-contracts/src/contracts/interfaces/ISignatureUtils.sol";
+import {IAVSDirectory} from "eigenlayer-contracts/src/contracts/interfaces/IAVSDirectory.sol";
+import {ECDSA} from "solady/utils/ECDSA.sol";
+
+contract SAMServiceManager {
+    // Function to Register Operator
+    // Function to Deregister Operator
+    // Create Sighting / Task
+    // Respond to Sighting / Task
+}
