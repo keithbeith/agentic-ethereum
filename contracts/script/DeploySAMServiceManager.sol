e79a589b1221d27f0b7f2e23c3ceb1938d78cbad
wr1159 <chuweiron18@gmail.com>
2025-02-06 19:07:52 +0000

    DeploySAMServiceManager: Scaffold deploy script

---
+++ contracts/script/DeploySAMServiceManager.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {SAMServiceManager} from "../src/SAMServiceManager.sol";
+import {IDelegationManager} from "eigenlayer-contracts/src/contracts/interfaces/IDelegationManager.sol";
+import {AVSDirectory} from "eigenlayer-contracts/src/contracts/core/AVSDirectory.sol";
+import {ISignatureUtils} from "eigenlayer-contracts/src/contracts/interfaces/ISignatureUtils.sol";
+
+contract DeploySAMServiceManager is Script {
+    address internal constant AVS_DIRECTORY = 0x055733000064333CaDDbC92763c58BF0192fFeBf
+    address internal constant DELEGATION_MANAGER = 0xA44151489861Fe9e3055d95adC98FbD462B948e7
+
+    address internal deployer;
+    address internal operator;
+    SAMServiceManager samServiceManager;
+}
